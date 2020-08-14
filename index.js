6c8c57f757a73f042af4a13a47d401cb478a6a60
thomasjmai22 <thomas.j.mai22@gmail.com>
2020-08-14 15:21:47 -0700

    final edits

---
+++ index.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/**
+ * Calls the dog api to randomly get a set number
+ * of images
+ */
+function getDogImage(inputValue) {
+  let requiredUrl = `https://dog.ceo/api/breeds/image/random/${inputValue}`;
+
+  fetch(requiredUrl)
+    .then((response) => response.json())
+    .then((responseJson) => displayResults(responseJson))
+    .catch((error) => alert("Something went wrong. Try again later."));
+}
+
+//displays images
+function displayResults(responseJson) {
+  console.log(responseJson);
+  let arrayOfImg = responseJson.message;
+  let display = getImages(arrayOfImg);
+  //replaces image
+  $(".results-img").html(display);
+}
+
+//Prepares a string of <img> elements and returns the same
+function getImages(arrayOfImg) {
+  let valueToReturn = "";
+  for (let i = 0; i < arrayOfImg.length; i++) {
+    valueToReturn += `<img src="${arrayOfImg[i]}" class="results-img">`;
+  }
+  return valueToReturn;
+}
+
+function watchForm() {
+  $("form").submit((event) => {
+    event.preventDefault();
+    let inputValue = $(".quantity").val();
+    getDogImage(inputValue);
+  });
+}
+
+$(function () {
+  console.log("App loaded! Waiting for submit!");
+  watchForm();
+});
